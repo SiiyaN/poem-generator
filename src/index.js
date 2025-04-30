09d2e2c98e899c13c55c5b29a30031bf260d947d
SiiyaN <87692603+SiiyaN@users.noreply.github.com>
2025-04-30 23:15:37 +0200

    Added the Ai API integration

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 function generatePoem(event) {
   event.preventDefault();
 
+  let apiKey = "f063aad8tb9d2a804775off7e6bf14bb";
+  let userInput = document.querySelector(".text-box").value;
+  let prompt = `Generate a short Zulu poem using the word "${userInput}".`;
+  let context =
+    "You are a creative ai who know how to rhyme with words, please generate a 4 line poem for every word submitted using the input form. Generate a new poem for every word submitted.Separate each line with a <br/>";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiURL).then(returnPoem);
+}
+
+function returnPoem(response) {
   new Typewriter("#poem", {
-    strings: "Poem goes here",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
