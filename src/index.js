98ac71167ad0eed05086a94ed525efd72e381641
SiiyaN <87692603+SiiyaN@users.noreply.github.com>
2025-04-30 23:23:32 +0200

    Added more instruction on the Ai context

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ function generatePoem(event) {
   let userInput = document.querySelector(".text-box").value;
   let prompt = `Generate a short Zulu poem using the word "${userInput}".`;
   let context =
-    "You are a creative ai who know how to rhyme with words, please generate a 4 line poem for every word submitted using the input form. Generate a new poem for every word submitted.Separate each line with a <br/>";
+    "You are a creative ai who know how to rhyme with words, please generate a 4 line Zulu poem for every word submitted using the input form. Generate a new poem for every word submitted.Separate each line with a <br/>, your lines must not have commas.";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   axios.get(apiURL).then(returnPoem);
