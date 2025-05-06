0127a48c5ce1f602e73c81203bdfcd2140bdc0b9
SiiyaN <87692603+SiiyaN@users.noreply.github.com>
2025-05-06 21:10:20 +0200

    Minor changes to the context.

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ function generatePoem(event) {
   let userInput = document.querySelector(".text-box").value;
   let prompt = `Generate a short Zulu poem using the word "${userInput}".`;
   let context =
-    "You are a creative ai who know how to rhyme with words, please generate a Zulu poem for every word submitted using the input form. Your poem MUST be 4 lines. Generate a new poem for every word submitted.Separate each line with a <br/>, your lines must not have commas.";
+    "You are a creative ai who know how to rhyme with words, please generate a Zulu poem for every word submitted using the input form. Your poem MUST be 4 lines. Generate a new 4 line poem poem for every word submitted.Separate each line with a <br/>, your lines must not have commas.";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   axios.get(apiURL).then(returnPoem);
